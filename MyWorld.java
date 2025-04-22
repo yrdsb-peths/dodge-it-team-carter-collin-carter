08131dd145710db6d75c7879623d32ca2d715883
vyin <limcolin0809@gmail.com>
2025-04-22 09:06:14 -0400

    Added bomb

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,11 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
-    public MyWorld() {
+    public MyWorld() 
+    {
         super(600, 400, 1);
+        
+        Bomb bomb = new Bomb();
+        addObject(bomb, 600, 100);
     }
 }
